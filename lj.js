aeb2b03dc45ef0d033de14fe3620257ad168ac0f
edgarrada <116873249+edgarrada@users.noreply.github.com>
2022-12-08 20:28:38 +0800

    Add files via upload

--- lj.js
+++ lj.js
@@ -1,2 +1,2 @@
-alert ('Zoom out mo pare kung naka zoom para maka kuha ka ng great experience')
-alert ('Try mo din mag scroll down kung tinatamad ka pindutin mo nalang yung "ABOUT" sa upper right')
+// alert ('Zoom out mo pare kung naka zoom para maka kuha ka ng great experience')
+// alert ('Try mo din mag scroll down kung tinatamad ka pindutin mo nalang yung "ABOUT" sa upper right')
